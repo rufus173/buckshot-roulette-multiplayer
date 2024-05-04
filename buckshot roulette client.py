4ec9de37d22735c29ec39015ea651df17ab837ed
rufus173 <derrickotron5000@gmail.com>
2024-05-04 19:44:47 +0100

    added defibrilator

--- buckshot roulette client.py
+++ buckshot roulette client.py
@@ -19,10 +19,10 @@ class gui():
         for i in range(8):
             self.player_1_items[i] = tkinter.Button(self.player_1_frame,fg=fg_1,bg=bg_2)
         for i in range(4):
-            self.player_1_items[i].grid(row=i,column=0,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+            self.player_1_items[i].grid(row=i+1,column=0,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
         self.player_1_frame.columnconfigure(0,weight=1)
         for i in range(4):
-            self.player_1_items[i+4].grid(row=i,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+            self.player_1_items[i+4].grid(row=i+1,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
         self.player_1_frame.columnconfigure(1,weight=1)
 
         #creating player 2s item window
@@ -33,22 +33,28 @@ class gui():
         for i in range(8):
             self.player_2_items[i] = tkinter.Button(self.player_2_frame,fg=fg_1,bg=bg_2)
         for i in range(4):
-            self.player_2_items[i].grid(row=i,column=0,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+            self.player_2_items[i].grid(row=i+1,column=0,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
         self.player_2_frame.columnconfigure(0,weight=1)
         for i in range(4):
-            self.player_2_items[i+4].grid(row=i,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+            self.player_2_items[i+4].grid(row=i+1,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
         self.player_2_frame.columnconfigure(1,weight=1)
 
         #creating the shotgun window
         self.shotgun_frame = tkinter.LabelFrame(self.root,text="shotgun",bg=bg_1)
         self.shotgun_frame.grid(row=0,column=1,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
-        self.player_2_select = tkinter.Button(self.shotgun_frame,text="player 2",fg="grey")
+        self.player_2_select = tkinter.Button(self.shotgun_frame,text="player 2",bg=bg_1)
         self.player_2_select.grid(row=0,column=0,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
-        self.player_1_select = tkinter.Button(self.shotgun_frame,text="yourself",fg="grey")
+        self.player_1_select = tkinter.Button(self.shotgun_frame,text="yourself",bg=bg_1)
         self.player_1_select.grid(row=1,column=0,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
         self.shotgun_frame.columnconfigure(0,weight=1)
         self.shotgun_frame.rowconfigure(0,weight=1)
         self.shotgun_frame.rowconfigure(1,weight=1)
+
+        #creating the defibrilator charges
+        player_2_defib = tkinter.Label(self.player_2_frame,text="⚡",bg="grey")
+        player_2_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
+        player_1_defib = tkinter.Label(self.player_1_frame,text="⚡",bg="grey")
+        player_1_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
 
         self.root.mainloop()
     def item_selected(self,item):
