947bbfd4636a9200abf16b243a16b02b36dc3155
rufus173 <derrickotron5000@gmail.com>
2024-05-04 19:15:33 +0100

    got both inventories and some of the shotgun window functional

--- buckshot roulette.py
+++ buckshot roulette.py
@@ -1,11 +1,16 @@
 import tkinter
 import socket
+from tkinter import font
 class gui():
     def __init__(self) -> None:#just gonna initialise the entire gui
         self.root = tkinter.Tk()
+        self.defaultFont = font.nametofont("TkDefaultFont") 
+        self.defaultFont.configure(size=19,weight=font.BOLD) 
         self.player_1_frame = tkinter.LabelFrame(self.root,text="you",bg="grey") #for all intents and purposes you are always player 1
         self.player_1_frame.grid(row=0,column=2,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
         self.root.columnconfigure(2,weight=1)
+
+        #creating your item window
         self.player_1_items = {}
         for i in range(8):
             self.player_1_items[i] = tkinter.Button(self.player_1_frame,fg="grey")
@@ -15,6 +20,28 @@ class gui():
         for i in range(4):
             self.player_1_items[i+4].grid(row=i,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
         self.player_1_frame.columnconfigure(1,weight=1)
+
+        #creating player 2s item window
+        self.player_2_frame = tkinter.LabelFrame(self.root,text="player 2",bg="grey")
+        self.player_2_frame.grid(row=0,column=0,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
+        self.root.columnconfigure(0,weight=1)
+        self.player_2_items = {}
+        for i in range(8):
+            self.player_2_items[i] = tkinter.Button(self.player_2_frame,fg="grey")
+        for i in range(4):
+            self.player_2_items[i].grid(row=i,column=0,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+        self.player_2_frame.columnconfigure(0,weight=1)
+        for i in range(4):
+            self.player_2_items[i+4].grid(row=i,column=1,sticky=tkinter.NSEW,columnspan=1,rowspan=1)
+        self.player_2_frame.columnconfigure(1,weight=1)
+
+        #creating the shotgun window
+        self.shotgun_frame = tkinter.LabelFrame(self.root,text="shotgun",bg="grey")
+        self.shotgun_frame.grid(row=0,column=1,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
+        self.player_2_select = tkinter.Button(self.shotgun_frame,text="you",fg="grey")
+        self.player_2_select.grid(row=0,column=0,columnspan=1,rowspan=1,sticky=tkinter.NSEW)
+        self.shotgun_frame.columnconfigure(0,weight=1)
+
         self.root.mainloop()
     def item_selected(self,item):
         pass
