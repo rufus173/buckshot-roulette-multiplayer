e9df00496f13350e538123898d34d983b4494662
rufus173 <derrickotron5000@gmail.com>
2024-05-05 19:41:11 +0100

    started bugfixing the updater

--- buckshot roulette client.py
+++ buckshot roulette client.py
@@ -13,6 +13,7 @@ class gui():
         fg_1 = "grey"
 
         self.root = tkinter.Tk()
+        self.root.title("buckshot roulette")
         self.defaultFont = font.nametofont("TkDefaultFont") 
         self.defaultFont.configure(size=19,weight=font.BOLD) 
         self.player_1_frame = tkinter.LabelFrame(self.root,text="you",bg=bg_1) #for all intents and purposes you are always player 1
@@ -56,10 +57,10 @@ class gui():
         self.shotgun_frame.rowconfigure(1,weight=1)
 
         #creating the defibrilator charges
-        player_2_defib = tkinter.Label(self.player_2_frame,text="⚡",bg="grey")
-        player_2_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
-        player_1_defib = tkinter.Label(self.player_1_frame,text="⚡",bg="grey")
-        player_1_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
+        self.player_2_defib = tkinter.Label(self.player_2_frame,text="⚡",bg="grey")
+        self.player_2_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
+        self.player_1_defib = tkinter.Label(self.player_1_frame,text="⚡",bg="grey")
+        self.player_1_defib.grid(row=0,column=0,columnspan=2,sticky=tkinter.NSEW)
 
         #this is threading the socket connection
         self.root.mainloop()
@@ -72,15 +73,29 @@ class connection(gui):
         self.server.connect((ip,8067))
         self.server.recv(1024)
 
-        self.lives = 0
-
+        self.p1_lives = 0
+        self.p2_lives = 0
+        self.p1_item_list = []
+        self.p2_item_list = []
         self.mainloop()
     def mainloop(self):
         while True:
             receive_buffer = self.server.recv(4096).decode()
             receive_buffer = receive_buffer.split(",")
             match receive_buffer[0]:
-                case "lives":
-                    self.lives = int(receive_buffer[1])
-                    print("updating lives",self.lives)
+                case "lives": #p1[1] then p2[2]
+                    self.p1_lives = int(receive_buffer[1])
+                    print(receive_buffer)
+                    self.p2_lives = int(receive_buffer[2])
+                    print("updating lives",self.p1_lives,self.p2_lives)
+                    self.player_1_defib["text"] = "⚡"*self.p1_lives
+                    self.player_2_defib["text"] = "⚡"*self.p2_lives
+                case "p1 items":
+                    self.p1_item_list = receive_buffer[1].split("~") #trust me this is easier
+                    for i in self.p1_item_list:
+                        self.player_1_items[i]["text"] = self.p1_item_list[i]
+                case "p2 items":
+                    self.p2_item_list = receive_buffer[1].split("~")
+                    for i in self.p2_item_list:
+                        self.player_2_items[i]["text"] = self.p2_item_list[i]
 connection(ip)
--- buckshot roulette server.py
+++ buckshot roulette server.py
@@ -5,11 +5,17 @@ class main:
     def __init__(self) -> None:
         server = socket.socket()
         server.bind(("192.168.1.239",8067))
+        
+        self.all_items = ["cigarettes","pills"]
+
         while True:
-            time.sleep(5)
+            time.sleep(1)
             self.lives = 2 + random.randint(0,2)
-            self.p1_lives,self.p2_lives = self.lives, self.lives
+            self.p1_lives = self.lives
+            self.p2_lives = self.lives
             self.turn = 0
+            self.p1_items = []
+            self.p2_items = []
             try:
                 print("opening socket for connections")
                 server.listen(2)
@@ -17,6 +23,7 @@ class main:
                 print("connection 1 found")
                 self.con2,address = server.accept()
                 print("connection 2 found")
+                time.sleep(1)
                 self.con1.sendall(b"_")
                 self.con2.sendall(b"_")
             except Exception as problem:
@@ -34,11 +41,33 @@ class main:
             except Exception as problem:
                 print(problem,"when trying to exit")
     def mainloop(self):
+        self.reload()
         while True:
             print("start of turn")
             self.turn += 1
             self.update_players()
             time.sleep(5)
     def update_players(self):#seperation with a comma
-        self.con1.sendall("lives,"+str(self.p1_lives).encode())
+        self.con1.sendall(("lives,"+str(self.p1_lives)+","+str(self.p2_lives)).encode()) #i should be better but 1st is p1 lives then p2 lives
+        self.con2.sendall(("lives,"+str(self.p1_lives)+","+str(self.p2_lives)).encode())
+        p1itemstring = ""
+        for i in self.p1_items:
+            p1itemstring = p1itemstring + i + "~" #to make it easier to reconstruct on the client side
+        p1itemstring = p1itemstring.rstrip("~")
+        p2itemstring = ""
+        for i in self.p2_items:
+            p2itemstring = p2itemstring + i + "~"
+        p2itemstring = p2itemstring.rstrip("~")
+        self.con1.sendall(("p1 items"+","+p1itemstring).encode())
+        self.con2.sendall(("p1 items"+","+p2itemstring).encode())
+        self.con1.sendall(("p2 items"+","+p1itemstring).encode())
+        self.con2.sendall(("p2 items"+","+p2itemstring).encode())
+    def reload(self):
+        for i in range(self.lives):
+            self.p1_items.append(self.all_items[random.randint(0,len(self.all_items)-1)])
+            self.p2_items.append(self.all_items[random.randint(0,len(self.all_items)-1)])
+            if len(self.p1_items) > 8:
+                self.p1_items = self.p1_items[:9]
+            if len(self.p2_items) > 8:
+                self.p2_items = self.p2_items[:9]
 main()
