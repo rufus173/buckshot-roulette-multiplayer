8c6a2e96f2108293e3d326077235979d06174c94
rufus173 <derrickotron5000@gmail.com>
2024-05-05 17:43:03 +0100

    fixed wierd binding issues with socket

--- buckshot roulette server.py
+++ buckshot roulette server.py
@@ -2,12 +2,12 @@ import socket
 import time
 class main:
     def __init__(self) -> None:
+        server = socket.socket()
+        server.bind(("192.168.1.239",8067))
         while True:
             time.sleep(5)
             try:
                 print("opening socket for connections")
-                server = socket.socket()
-                server.bind(("192.168.1.239",8067))
                 server.listen(2)
                 con1, address = server.accept()
                 print("connection 1 found")
@@ -18,17 +18,18 @@ class main:
             except Exception as problem:
                 print(problem)
                 try:
-                    server.close()
-                    server.shutdown()
+                    con1.shutdown(socket.SHUT_RDWR)
+                    con2.shutdown(socket.SHUT_RDWR)
                 except:
                     print("socket could not be shut down")
 
                         
             
             try:
-                server.close()
-                server.shutdown(socket.SHUT_RDWR)
-            except:
-                pass
+                con1.shutdown(socket.SHUT_RDWR)
+                con2.shutdown(socket.SHUT_RDWR)
+                print("closed the sockets")
+            except Exception as problem:
+                print(problem,"when trying to exit")
 
 main()
